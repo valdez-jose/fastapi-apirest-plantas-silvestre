450e708b7816cf9b852acfcf9202fba56c8d4964
valdez-jose <tu-email-de-github@example.comclear>
2026-06-06 17:42:58 -0300

    fix: update CORS origins

--- app/main.py
+++ app/main.py
@@ -20,6 +20,8 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "http://127.0.0.1:5501",
+        "http://localhost:5501",
     ],
     allow_credentials=True,
     allow_methods=["*"],
